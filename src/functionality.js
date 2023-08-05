fd78ebd44befc515eac0205e4264a4560bc06318
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-08-05 14:34:28 +0300

    Updated the js files

--- src/functionality.js
+++ src/functionality.js
@@ -1,64 +1,24 @@
-/* eslint-disable no-console */
-// Function to fetch leaderboard data from the API and populate the table
-async function fetchLeaderboard() {
-  try {
-    // eslint-disable-next-line no-use-before-define
-    const data = await fetchLeaderboardFromAPI();
-    const leaderboardBody = document.getElementById('leaderboard-body');
-    leaderboardBody.innerHTML = ''; // Clear existing data
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
 
-    data.forEach((entry) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-          <td>${entry.name}</td>
-          <td>${entry.score}</td>
-        `;
-      leaderboardBody.appendChild(row);
-    });
-  } catch (error) {
-    console.error('Error fetching leaderboard data:', error);
-  }
-}
-
-// Function to fetch leaderboard data from the API (Mock data for this example)
-function fetchLeaderboardFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const mockData = [
-        { name: 'Player 1', score: 1000 },
-        { name: 'Player 2', score: 900 },
-        { name: 'Player 3', score: 800 },
-        { name: 'Player 4', score: 700 },
-        { name: 'Player 5', score: 600 },
-      ];
-      resolve(mockData);
-    }, 1000);
-  });
-}
+// Function to fetch leaderboard data from the API
+const fetchLeaderboardFromAPI = async () => {
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  return data.result;
+};
 
 // Function to submit a player's score to the local leaderboard
-function submitScore(name, score) {
-  try {
-    // eslint-disable-next-line no-use-before-define
-    const leaderboardData = getLocalLeaderboard();
-    leaderboardData.push({ name, score });
-    localStorage.setItem('leaderboardData', JSON.stringify(leaderboardData));
-    console.log('Score submitted successfully');
-    fetchLeaderboard(); // Refresh the leaderboard after submission
-    document.getElementById('name-input').value = ''; // Clear the name input field after submission
-    document.getElementById('score-input').value = ''; // Clear the score input field after submission
-  } catch (error) {
-    console.error('Error submitting score:', error);
-  }
-}
-
-// Function to get the local leaderboard data from localStorage
-function getLocalLeaderboard() {
-  const leaderboardData = JSON.parse(localStorage.getItem('leaderboardData'));
-  return leaderboardData || [];
-}
+const submitScore = async (name, score) => {
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+    body: JSON.stringify({ user: name, score }),
+  });
+  const data = await res.json();
+  return data.result;
+};
 
 export {
   submitScore,
-  fetchLeaderboard,
+  fetchLeaderboardFromAPI,
 };
